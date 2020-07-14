a30434dba07fd8a69422664482d2e5c338fc2d45
Arturo cortijo Purizaca <arts084@gmail.com>
2020-07-13 21:54:02 -0400

    Using the useState() Hook for State Manipulation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React, { Component } from 'react';
+// useState is the hook that allows us to manage state in a functional component.
+import React, { useState } from 'react';
 import Person from './Person/Person';
 
 import './App.css';
 
-class App extends Component {
-  // this state property here is only available like this in components that extend components
-  // We initialize it by assigning a value and this value is a Javascript object.
-  state = {
+const App = (props) => {
+  // useState returns an array with exactly two elements and always two elements
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: 'Art', age: 36 },
-      { name: 'Allie', age: 30 },
-      { name: 'Sara', age: 31 },
+      { name: 'Steve', age: 30 },
+      { name: 'Jason', age: 31 },
     ],
-  };
+    // otherState: 'some other value',
+  });
 
-  switchNameHandler = () => {
-    // Don't do this : this.state.persons[0].name = 'Pedro';
-    // set state takes an object as an argument and it will merge whatever we define here with our existing state.
-    // This will update only what was modified
-    this.setState({
+  const [otherState, setOtherState] = useState('some other value');
+
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
+    setPersonsState({
       persons: [
         { name: 'Pedro', age: 47 },
         { name: 'Allie', age: 30 },
@@ -27,29 +29,27 @@ class App extends Component {
     });
   };
 
-  render() {
-    return (
-      <div className='App'>
-        <h1>Hey you</h1>
-        {/* The first part Switch Name is totally up to you but you typically use handler here to indicate that this is a method you're not actively calling but you're assigning as an event handler. This pattern is not required but it's not required */}
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        >
-          My hobbies: COD
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className='App'>
+      <h1>Hey you</h1>
+      {/* Because we're no longer inside of a class anymore, the keyword this (from this.switchNameHandler is no longer necessary) */}
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      >
+        My hobbies: COD
+      </Person>
+      <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age}
+      />
+    </div>
+  );
+};
 
 export default App;
