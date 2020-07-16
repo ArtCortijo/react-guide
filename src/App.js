f99d149ce83b5269dc823fa0c33da74aa84d77ab
Arturo cortijo Purizaca <arts084@gmail.com>
2020-07-16 13:48:46 -0400

    Flexible Lists & Keys

--- src/App.js
+++ src/App.js
@@ -5,34 +5,44 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 },
+      { id: 'asda', name: 'Max', age: 28 },
+      { id: '32fw', name: 'Manu', age: 29 },
+      { id: 'sdf3', name: 'Stephanie', age: 26 },
     ],
     otherState: 'some other value',
     showPersons: true,
   };
 
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked!');
-    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 },
-      ],
-    });
+  deletePersonHandler = (personIndex) => {
+    // A good practice is to create a copy of your persons array before manipulating it and a simple way of doing this is by calling the slice method. Slice without arguments simply copies the full array and returns a new one
+    // const persons = this.state.persons.slice();
+
+    // or you can you use the spread operator
+    const persons = [...this.state.persons];
+
+    // *** You should always update state in an immutable fashion, so without mutating the original state first. ***
+
+    // Splicing a person index and splice one element. This simply removes one element from the array
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
   };
 
-  nameChangeHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 27 },
-      ],
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((pers) => {
+      return pers.id === id;
     });
+
+    // The better approach is to create a new javascript object like this and then use this spread operator in front of the object I'm fetching. Just like with the array it's also available for objects and it will distribute all the properties of the object we fetch here into this new object we're creating here.
+    const person = {
+      ...this.state.persons[personIndex],
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
   };
 
   togglePersonsHandler = () => {
@@ -59,8 +69,16 @@ class App extends Component {
         <div>
           {/* Outputting lists */}
           {/* Map simply maps every element in a given array such as our persons array here into something else */}
-          {this.state.persons.map((person) => {
-            return <Person name={person.name} age={person.age} />;
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangeHandler(event, person.id)}
+              />
+            );
           })}
         </div>
       );
